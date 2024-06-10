762ee4f26cc209840122d6f38cd482fb73fa8fdf
Sebastian835 <77905147+Sebastian835@users.noreply.github.com>
2024-06-10 14:53:18 -0500

    View Upload Receipt
    The view was created to be able to upload the receipt, with the image and pdf preview, additionally the call was made in the app-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UploadReceiptComponent } from './views/upload-receipt/upload-receipt.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UploadReceiptComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/views/upload-receipt/upload-receipt.component.ts
+++ src/app/views/upload-receipt/upload-receipt.component.ts
@@ -1,12 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-upload-receipt',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './upload-receipt.component.html',
-  styleUrl: './upload-receipt.component.scss'
+  styleUrl: './upload-receipt.component.scss',
 })
 export class UploadReceiptComponent {
+  fileUrl: SafeResourceUrl | null = null;  
+  isImage: boolean = false;
 
+  constructor(private sanitizer: DomSanitizer) {}
+
+  onFileSelected(event: any): void {
+    const file: File = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.fileUrl = this.sanitizer.bypassSecurityTrustResourceUrl(reader.result as string);
+        this.isImage = file.type.startsWith('image/');
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  onSubmit(): void {
+    console.log('Archivo subido:', this.fileUrl);
+  }
 }
