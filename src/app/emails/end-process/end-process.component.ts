fd684fd8b1112bc71a23c1e819c971492dd1d637
anyel-ec <paulangel826@gmail.com>
2024-06-09 22:41:05 -0500

    add components to split tasks

---
+++ src/app/emails/end-process/end-process.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-end-process',
+  standalone: true,
+  imports: [],
+  templateUrl: './end-process.component.html',
+  styleUrl: './end-process.component.scss'
+})
+export class EndProcessComponent {
+
+}
---
+++ src/app/views/upload-receipt/upload-receipt.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-receipt',
+  standalone: true,
+  imports: [],
+  templateUrl: './upload-receipt.component.html',
+  styleUrl: './upload-receipt.component.scss'
+})
+export class UploadReceiptComponent {
+
+}
