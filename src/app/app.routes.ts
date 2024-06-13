cd1a3d7674a837f6ab738d32e79931a50f1b3e75
César Zambrano <zamcega1804@hotmail.com>
2024-06-13 17:34:47 -0500

    Actualización de las rutas al iniciar el servidor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { UploadReceiptComponent } from './views/upload-receipt/upload-receipt.component';
 import { RegistrationFormComponent } from './views/registration-form/registration-form.component';
 import { NgModule } from '@angular/core';
+import { LoginComponent } from './views/login/login.component';
 
 
 
@@ -13,6 +14,13 @@ export const routes: Routes = [
   {
     path: 'registro-aspirantes',
     component: RegistrationFormComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: '', redirectTo: '/registro-aspirantes', pathMatch: 'full'
   }
 ];
 
--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavComponent } from '../../components/nav/nav.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [NavComponent, FooterComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
