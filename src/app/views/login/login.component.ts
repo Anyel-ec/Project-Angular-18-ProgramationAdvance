2b66dd812a9e3ed31230094599ab99192f8dea4a
César Zambrano <zamcega1804@hotmail.com>
2024-06-13 20:48:05 -0500

    semi complete login form and agreggate css styles in nav and login

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavComponent } from '../../components/nav/nav.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [NavComponent, FooterComponent],
+  imports: [NavComponent, FooterComponent, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
