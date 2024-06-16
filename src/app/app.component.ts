007e7a6d9879014f6a31d175154e22cd77212e9f
César Zambrano <zamcega1804@hotmail.com>
2024-06-15 20:27:31 -0500

    Add nav-bar and footer in app.component.ts, update the nav.html and updated the ts files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { UploadReceiptComponent } from './views/upload-receipt/upload-receipt.component';
 import { CommonModule } from '@angular/common';
 import { RegistrationFormComponent } from './views/registration-form/registration-form.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UploadReceiptComponent, CommonModule, RegistrationFormComponent],
+  imports: [RouterOutlet, UploadReceiptComponent, CommonModule, RegistrationFormComponent, NavComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
