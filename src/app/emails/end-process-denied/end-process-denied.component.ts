17c4a2a9ad1aefb2c899fdb37309250282b2405c
César Zambrano <zamcega1804@hotmail.com>
2024-06-16 23:21:15 -0500

    create component verify-data-denied and end-process-denied

---
+++ src/app/emails/end-process-denied/end-process-denied.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-end-process-denied',
+  standalone: true,
+  imports: [],
+  templateUrl: './end-process-denied.component.html',
+  styleUrl: './end-process-denied.component.scss'
+})
+export class EndProcessDeniedComponent {
+
+}
---
+++ src/app/emails/verify-data-denied/verify-data-denied.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-verify-data-denied',
+  standalone: true,
+  imports: [],
+  templateUrl: './verify-data-denied.component.html',
+  styleUrl: './verify-data-denied.component.scss'
+})
+export class VerifyDataDeniedComponent {
+
+}
