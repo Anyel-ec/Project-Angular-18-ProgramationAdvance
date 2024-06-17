09b4648a8a43cff8ceb28e3b438c99d25e98dce6
César Zambrano <zamcega1804@hotmail.com>
2024-06-17 00:26:30 -0500

    Update router fot new component emails

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { RouterModule, Routes } from '@angular/router';
 import { UploadReceiptComponent } from './views/upload-receipt/upload-receipt.component';
 import { RegistrationFormComponent } from './views/registration-form/registration-form.component';
+//import { RegistrationFormComponent } from './emails/registration-form/registration-form.component';
 import { VerifyPaymentsComponent } from './views/verify-data/verify-payments.component';
 import { NgModule } from '@angular/core';
 import { LoginComponent } from './views/login/login.component';
 import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
+import { VerifyDataComponent } from './emails/verify-data/verify-data.component';
+import { VerifyDataDeniedComponent } from './emails/verify-data-denied/verify-data-denied.component';
+import { EndProcessComponent } from './emails/end-process/end-process.component';
+import { EndProcessDeniedComponent } from './emails/end-process-denied/end-process-denied.component';
 
 export const routes: Routes = [
   {
@@ -19,6 +24,33 @@ export const routes: Routes = [
     path: 'registro-aspirantes',
 
     component: RegistrationFormComponent
+  },
+  /*
+  {
+    path: 'email-registro-datos',
+
+    component: RegistrationFormComponent
+  },
+  */
+  {
+    path: 'email-datos-aceptados',
+
+    component: VerifyDataComponent
+  },
+  {
+    path: 'email-datos-rechazados',
+
+    component: VerifyDataDeniedComponent
+  },
+  {
+    path: 'email-comprobante-aceptado',
+
+    component: EndProcessComponent
+  },
+  {
+    path: 'email-comprobante-rechazado',
+
+    component: EndProcessDeniedComponent
   },
   {
     path: 'login',
