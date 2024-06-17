feb919c3364f0d45cd26e10bf16f3a32ea0fa139
César Zambrano <zamcega1804@hotmail.com>
2024-06-16 23:17:30 -0500

    add sends date email and apply styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-<<<<<<< HEAD
 import { RegistrationFormComponent } from './views/registration-form/registration-form.component';
 import { NavComponent } from './components/nav/nav.component';
 import { FooterComponent } from './components/footer/footer.component';
-=======
->>>>>>> 90d12b9c2e64b9b5844fa527427a8918c1ef43fb
+import { UploadReceiptComponent } from './views/upload-receipt/upload-receipt.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-<<<<<<< HEAD
   imports: [RouterOutlet, UploadReceiptComponent, CommonModule, RegistrationFormComponent, NavComponent, FooterComponent],
-=======
-  imports: [RouterOutlet, CommonModule ],
->>>>>>> 90d12b9c2e64b9b5844fa527427a8918c1ef43fb
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
